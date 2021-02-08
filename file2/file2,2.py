bd584c359d188256b34a40f580cdf7585cc3b9ff
Bogdan <bog10an@gmail.com>
2021-02-08 14:36:40 +0200

    file2

---
+++ file2/file2,2.py
@@ -0,0 +1,11 @@
+numbers = [1,2,3]
+strings = ["hello","world"]
+names = ["John", "Eric", "Jessica"]
+
+# write your code here
+second_name = names[1]
+
+# this code should write out the filled arrays and the second name in the names list (Eric).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
