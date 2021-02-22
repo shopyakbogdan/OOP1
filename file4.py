444fb7963d2c72e84a37971f4436cce91932fe08
Bogdan <bog10an@gmail.com>
2021-02-22 14:00:41 +0200

    file4

--- file4.py
+++ file4.py
@@ -5,6 +5,7 @@ def list_benefits():
 def build_sentence(benefit):
     return "%s is a benefit of functions!" % benefit
 
+
 def name_the_benefits_of_functions():
     list_of_benefits = list_benefits()
     for benefit in list_of_benefits:
